3c31516522ea59de35900bd1bc424b432f3969da
Artforteam <artforteam2018@gmail.com>
2019-01-31 17:00:52 +0500

    s

--- server.js
+++ server.js
@@ -864,6 +864,7 @@ io.on('connection', client => {
         checkTokenWs(data)
             .then(result => {
                 if (result.success) {
+                    console.log(data.region.split(','))
                     let queryRules = {
                         text: queries.getTableQuery,
                         values: [data.region.split(',')]
